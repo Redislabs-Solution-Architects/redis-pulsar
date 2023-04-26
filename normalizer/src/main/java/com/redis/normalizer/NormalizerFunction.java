3a77aa1f47ebf5e4f4a299a1452ddae461b0bd55
joeywhelan <joey.whelan@redis.com>
2023-04-26 12:13:50 -0600

    Fix documentation

--- normalizer/src/main/java/com/redis/normalizer/NormalizerFunction.java
+++ normalizer/src/main/java/com/redis/normalizer/NormalizerFunction.java
@@ -5,11 +5,24 @@ import org.apache.pulsar.functions.api.Function;
 import org.json.JSONObject;
 import java.nio.charset.StandardCharsets;
 
+/*
+ * Implements a custom Pulsar function for manipulating metric data passed as JSON.
+ */
 public class NormalizerFunction implements Function<byte[], byte[]> {
     private double M1_MIN = 5.0;
     private double M1_MAX = 50.0;
     private double M2_CONV = 133.0;
 
+    /*
+     * This function accepts a JSON object that is encoded as a byte array.  One field in that object represents one
+     * of three numeric metric types for the given stock symbol.  This function performs some contrived scaling 
+     * operations on that metric and then writes new JSON object (as a byte array) to a new topic that is being monitored
+     * by a Pulsar Sink that subsequently writes the JSON to Redis.
+     * 
+     * @param input     JSON object containing a Redis key name, JSON path, and value object representing a
+     *                  a stock ticker symbol (string) and a metric (numeric).
+     * @return          Modified JSON object encoded as a byte array.
+     */
     @Override
     public byte[] process(byte[] input, Context context) {
         JSONObject in = new JSONObject(new String(input, StandardCharsets.UTF_8));
@@ -20,7 +33,7 @@ public class NormalizerFunction implements Function<byte[], byte[]> {
         
         JSONObject val = new JSONObject();
         String metric = key.split(":")[2];
-        switch (metric) {
+        switch (metric) {   //3 types of metrics, each with their own contrived scaling transformation
             case "M1":
                 double m1 = (double) (value.getInt("M1") - M1_MIN)/(M1_MAX - M1_MIN);
                 m1 = Math.round(m1 * 100.0)/100.0;
@@ -42,6 +55,6 @@ public class NormalizerFunction implements Function<byte[], byte[]> {
         out.put("path", path);
         out.put("value", val);
         context.getLogger().info("Writing - key: " + key + " value: " + val + " path: " + path);
-        return out.toString().getBytes(StandardCharsets.UTF_8);
+        return out.toString().getBytes(StandardCharsets.UTF_8);  //write out the JSON object, as byte array, to next step in pipeline
     }  
 }
