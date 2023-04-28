cc2608c6875fba98fbc3a4289ac7714f848e880d
joeywhelan <joey.whelan@redis.com>
2023-04-28 11:31:58 -0600

    fix build script

--- util/build.sh
+++ util/build.sh
@@ -5,4 +5,6 @@
 
 cd ../normalizer && mvn clean package && cd -
 cd ../rjsonsink && mvn clean package && cd -
-cd ../gear && npm run build && cd -
+cd ../gear && npm install && npm run build && cd -
+cd ../consumer && npm install && cd -
+cd ../producer && npm install && cd -
